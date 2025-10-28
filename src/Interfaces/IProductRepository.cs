aa77ef51884edaa311b5e8a784b8143adde3168a
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2025-10-27 23:30:58 -0300

    Interface Product

---
+++ src/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using BugStore.Models;
+
+namespace BugStore.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<bool> Create(Product product);
+        Task<bool?> Delete(Product product);
+        Task<List<Product?>> Get();
+        Task<Product?> GetById(Guid id);
+        Task<bool> Update(Product product);
+    }
+}
