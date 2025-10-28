33cc9c861face02ea330726dfd144ce2a4f4ed15
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2025-10-27 23:37:50 -0300

    Implementação da Interface Customer em CustomerRepository

--- src/Interfaces/ICustomerRepository.cs
+++ src/Interfaces/ICustomerRepository.cs
@@ -4,8 +4,8 @@ namespace BugStore.Interfaces
 {
     public interface ICustomerRepository
     {
-        Task<bool> Create(Customer customer);
-        Task<bool?> Delete(Customer customer);
+        Task Create(Customer customer);
+        Task Delete(Customer customer);
         Task<List<Customer?>> Get();
         Task<Customer?> GetById(Guid id);
         Task<bool> Update(Customer customer);
---
+++ src/Repositories/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using BugStore.Data;
+using BugStore.Interfaces;
+using BugStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugStore.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public CustomerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Customer customer)
+        {
+           await _context.Customers.AddAsync(customer);
+        }
+
+        public async Task Delete(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+        public async Task<List<Customer?>> Get()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public Task<Customer?> GetById(Guid id)
+        {
+            if(id == Guid.Empty)
+                return Task.FromResult<Customer?>(null);
+            return _context.Customers.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public Task<bool> Update(Customer customer)
+        {
+            if (customer == null || customer.Id == Guid.Empty)
+                return Task.FromResult(false);
+            _context.Customers.Update(customer);
+            return Task.FromResult(true);
+        }
+    }
+}
