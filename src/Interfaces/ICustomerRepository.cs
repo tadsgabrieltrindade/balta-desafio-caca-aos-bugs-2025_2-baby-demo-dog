3279210f59e19459a9cf4ba8eda254dafe03f89e
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2025-10-27 23:27:56 -0300

    Interface Customer

---
+++ src/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using BugStore.Models;
+
+namespace BugStore.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<bool> Create(Customer customer);
+        Task<bool?> Delete(Customer customer);
+        Task<List<Customer?>> Get();
+        Task<Customer?> GetById(Guid id);
+        Task<bool> Update(Customer customer);
+    }
+}
